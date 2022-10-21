c69e0b99abcc6758f5f69c99947a61b8d8998fad
Swadesh Shivam <swadeshshivam12345@gmail.com>
2022-10-21 20:11:12 +0530

    Next commit done while updating to github repository

--- index.js
+++ index.js
@@ -102,7 +102,20 @@ app.listen(port, function (err) {
   console.log(`Yup.! My express server is up and running on port: ${port}`);
 });
 
+app.listen(port, function (err) {
+    if (err) {
+      console.log(`Error in running the server:  ${err}`);
+      return;
+    }
+  
+    console.log(`Yup.! My express server is up and running on port: ${port}`);
+  });s
+
 
 
 // github repository for socialVerse
 //   <--- https://github.com/Swadeshshivam123/socialVerse-app.git  --->
+ 
+//Pushing changes
+//   <--- git push https://github.com/Swadeshshivam123/socialVerse-app.git master  --->
+
